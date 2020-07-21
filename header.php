23abb5c086766fa777967db182f6fbae85ee2c8d
Pavan Surya Prakash Ponnam <pponnam@ap.logicalis.com>
2020-07-21 17:30:37 +0800

    Update Color

--- header.php
+++ header.php
@@ -162,7 +162,7 @@
 		<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
 		<link rel="manifest" href="site.webmanifest">
-		<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+		<link rel="mask-icon" href="safari-pinned-tab.svg" color="#ff0000">
 		<meta name="msapplication-TileColor" content="#ff0000">
 		<meta name="theme-color" content="#ffffff">
 		
